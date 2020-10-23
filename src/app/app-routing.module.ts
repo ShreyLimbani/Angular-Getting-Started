93fae50c61708ff0380b72f29dcc568c7e36929e
Shrey Limbani <shreylimbani@gmail.com>
2020-10-23 13:19:19 +0530

    Modified App Routing TS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,8 +10,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent },
-  { path: 'messages', component: MessagesComponent },
+  { path: 'heroes', component: HeroesComponent }  
 ];
 
 @NgModule({
